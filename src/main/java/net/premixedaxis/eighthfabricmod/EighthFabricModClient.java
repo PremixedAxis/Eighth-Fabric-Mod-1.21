f1110e862956fcb1db098b8da39d382908b31406
PremixedAxis <cameron.yutzy@gmail.com>
2024-12-17 12:46:54 -0600

    Initial commit

---
+++ src/main/java/net/premixedaxis/eighthfabricmod/EighthFabricModClient.java
@@ -0,0 +1,10 @@
+package net.premixedaxis.eighthfabricmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EighthFabricModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/premixedaxis/eighthfabricmod/EighthFabricModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.premixedaxis.eighthfabricmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EighthFabricModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
